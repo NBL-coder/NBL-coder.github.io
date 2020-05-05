737da1965d9680721114c416ac871e4a567a046b
NBL coder <nblmusicproductions.com>
2020-05-05 15:24:18 +0700

    update 5/5/2020

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const navslide = () =>{
     const triangle = document.querySelector('.triangle');
-    const nav = document.querySelector('.left');
+    const nav = document.querySelector('.menu');
+    const nav2=document.querySelector('.right');
     triangle.addEventListener('click',() => {
-        nav.classList.toggle('nav-active');
-    });
+        nav.classList.toggle('nav-active');});
+    triangle.addEventListener('click',() => {
+        nav2.classList.toggle('nav-active2');});
 }
 navslide();
