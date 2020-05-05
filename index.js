66e258addca7b92cb8d5a05b744b0bc8c1dcc024
NBL coder <nblmusicproductions.com>
2020-05-05 18:26:57 +0700

    shit

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
 const navslide = () =>{
     const triangle = document.querySelector('.triangle');
     const nav = document.querySelector('.menu');
-    const nav2=document.querySelector('.right');
+    const nav2 =document.querySelector('.right');
     triangle.addEventListener('click',() => {
         nav.classList.toggle('nav-active');});
     triangle.addEventListener('click',() => {
         nav2.classList.toggle('nav-active2');});
+
+
+    
+}
+const loadin =() =>
+{
+    const links = document.querySelectorAll('.menu_2 li');
+        links.forEach((link,index) => 
+        {
+            link.style.animation ='listfade 0.5s ease forwards ${index / 10 + 2}s';
+            console.log(index /10)
+        });    
+
 }
 navslide();
+loadin();
