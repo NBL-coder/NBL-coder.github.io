79bb0326a34b0e584c0b1432356f9a8e312547b0
NBL coder <nblmusicproductions.com>
2020-05-07 17:07:56 +0700

    nav

--- index.js
+++ index.js
@@ -8,4 +8,30 @@ const navslide = () =>{
         nav2.classList.toggle('nav-active2');});
 }
 navslide();
+const scrol = document.querySelector('.nav-bar');
+// console.log(window.innerHeight);
+
+window.addEventListener('scroll',() => {
+    console.log(scrol.getBoundingClientRect().top);
+    if(scrol.getBoundingClientRect().top == 0)
+    {
+        scrol.classList.add('nav-scroll');
+    }
+    else
+    {
+        scrol.classList.remove('nav-scroll');
+    }
+});
+const easter = document.querySelector('.nav-link_2')
+console.log(easter);
+window.addEventListener('scroll',()=>{
+    if(scrol.getBoundingClientRect().top == 0)
+    {
+        easter.classList.add('easter-scroll');
+    }
+    else{
+        easter.classList.remove('easter-scroll');
+    }
+});
+
 
